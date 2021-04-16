ea873dc0d728a36c59fd1b912380bd2718bd68a5
Ben Doughton <bdoughton@me.com>
2021-04-16 14:20:00 +0100

    updated Congnito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-2_w04Wt0zi0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4f992nqjfs6bs4hjp9cs21lp9a', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
